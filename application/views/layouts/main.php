260007931cf9456c0bf676159fc349d574d786ba
Adolf Mrls <fito-morales@hotmail.com>
2019-01-21 15:02:57 -0400

    sesion VENTAS mejorado

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -71,7 +71,7 @@
                             <!-- Menu Footer-->
                             <li class="user-footer">
                                 <div class="pull-left">
-                                    <a href="<?php site_url()?>dashb/cuenta" class="btn btn-default btn-flat">Mi Cuenta</a>
+                                    <a href="<?php echo site_url()?>admin/dashb/cuenta" class="btn btn-default btn-flat">Mi Cuenta</a>
                                 </div>
                                 <div class="pull-right">
                                     <a href="<?php echo site_url()?>admin/dashb/logout" class="btn btn-default btn-flat">Salir</a>
