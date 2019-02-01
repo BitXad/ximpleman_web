67cc86daec2aaf503996c0db2945f5d96fde4df9
Carlos Soto <masterbity@gamil.com>
2019-02-01 16:23:21 -0400

    Cambios en el controlador cliente

--- application/controllers/Cliente.php
+++ application/controllers/Cliente.php
@@ -25,16 +25,16 @@ class Cliente extends CI_Controller{
 
         $data['a'] = $a;
         $data['err'] ="";
-        $data['cliente'] = $this->Cliente_model->get_all_cliente();
+        $data['cliente'] = $this->Cliente_model->get_cliente_all();
         
         $this->load->model('Tipo_cliente_model');
-        $data['all_tipo_cliente'] = $this->Tipo_cliente_model->get_all_tipo_cliente();
+        $data['all_tipo_cliente'] = $this->Tipo_cliente_model->get_all_tipo_cliente_asc();
         
         $this->load->model('Categoria_cliente_model');
-        $data['all_categoria_cliente'] = $this->Categoria_cliente_model->get_all_categoria_cliente();
+        $data['all_categoria_cliente'] = $this->Categoria_cliente_model->get_all_categoria_cliente_asc();
         
         $this->load->model('Categoria_clientezona_model');
-        $data['all_categoria_clientezona'] = $this->Categoria_clientezona_model->get_all_categoria_clientezona();
+        $data['all_categoria_clientezona'] = $this->Categoria_clientezona_model->get_all_categoria_clientezona_asc();
         
         $this->load->model('Usuario_model');
         $data['all_usuario'] = $this->Usuario_model->get_all_usuario();
@@ -65,7 +65,7 @@ class Cliente extends CI_Controller{
 
             //$this->form_validation->set_rules('cliente_codigo','Cliente Codigo','required');
             $this->form_validation->set_rules('cliente_nombre','Cliente Nombre','required');
-            $this->form_validation->set_rules('cliente_nombrenegocio','Nombre Negocio','required');
+            //$this->form_validation->set_rules('cliente_nombrenegocio','Nombre Negocio','required');
 
             if($this->form_validation->run())     
             {
@@ -122,14 +122,14 @@ class Cliente extends CI_Controller{
                     }
             /* *********************FIN imagen***************************** */
                 $usuario_id = $session_data['usuario_id'];
-                $mifecha = $this->Cliente_model->normalize_date($this->input->post('cliente_aniversario'));
+                //no es necesario porque se esta usando el date y ya no el date piker
+                //$mifecha = $this->Cliente_model->normalize_date($this->input->post('cliente_aniversario'));
                 $estado_id = 1;
                 $params = array(
                             'estado_id' => $estado_id,
                             'tipocliente_id' => $this->input->post('tipocliente_id'),
                             'categoriaclie_id' => $this->input->post('categoriaclie_id'),
                             'cliente_codigo' => $this->input->post('cliente_codigo'),
-                            'categoriacliezona_id' => $this->input->post('categoriacliezona_id'),
                             'cliente_nombre' => $this->input->post('cliente_nombre'),
                             'cliente_ci' => $this->input->post('cliente_ci'),
                             'cliente_direccion' => $this->input->post('cliente_direccion'),
@@ -138,12 +138,13 @@ class Cliente extends CI_Controller{
                             'cliente_foto' => $foto,
                             'cliente_email' => $this->input->post('cliente_email'),
                             'cliente_nombrenegocio' => $this->input->post('cliente_nombrenegocio'),
-                            'cliente_aniversario' => $mifecha,
+                            'cliente_aniversario' => $this->input->post('cliente_aniversario'),
                             'cliente_latitud' => $this->input->post('cliente_latitud'),
                             'cliente_longitud' => $this->input->post('cliente_longitud'),
                             'cliente_nit' => $this->input->post('cliente_nit'),
                             'cliente_razon' => $this->input->post('cliente_razon'),
                             'usuario_id' => $this->input->post('usuario_id'),
+                            'zona_id' => $this->input->post('zona_id'),
                 );
             
             $cliente_id = $this->Cliente_model->add_cliente($params);
@@ -155,15 +156,15 @@ class Cliente extends CI_Controller{
 			$data['all_estado'] = $this->Estado_model->get_all_estado_activo_inactivo();
                         
                         $this->load->model('Categoria_clientezona_model');
-                        $data['all_categoria_clientezona'] = $this->Categoria_clientezona_model->get_all_categoria_clientezona();
+                        $data['zona'] = $this->Categoria_clientezona_model->get_all_categoria_clientezona();
                         /***Añadido por Mario Escobar parqa asignarle a un usuario prevendedor***/
                         $this->load->model('Usuario_model');
 			$data['all_usuario_prev'] = $this->Usuario_model->get_all_usuario_prev_activo();
 
 			$this->load->model('Tipo_cliente_model');
 			$data['all_tipo_cliente'] = $this->Tipo_cliente_model->get_all_tipo_cliente();
-
-			$this->load->model('Categoria_cliente_model');
+			
+                        $this->load->model('Categoria_cliente_model');
 			$data['all_categoria_cliente'] = $this->Categoria_cliente_model->get_all_categoria_cliente();
 			
             
@@ -199,7 +200,7 @@ class Cliente extends CI_Controller{
 
 			//$this->form_validation->set_rules('cliente_codigo','Cliente Codigo','required');
 			$this->form_validation->set_rules('cliente_nombre','Cliente Nombre','required');
-                        $this->form_validation->set_rules('cliente_nombrenegocio','Cliente Nombre Negocio','required');
+                        //$this->form_validation->set_rules('cliente_nombrenegocio','Cliente Nombre Negocio','required');
 		
 	    if($this->form_validation->run())     
             {
@@ -268,14 +269,14 @@ class Cliente extends CI_Controller{
                 }
             /* *********************FIN imagen***************************** */
                             //$this->input->post('cliente_foto'),
-                           $mifecha = $this->Cliente_model->normalize_date($this->input->post('cliente_aniversario'));
+                           //$mifecha = $this->Cliente_model->normalize_date($this->input->post('cliente_aniversario'));
                             //$mifecha = normalize_date($this->input->post('cliente_aniversario'));
                 $params = array(
 					'estado_id' => $this->input->post('estado_id'),
 					'tipocliente_id' => $this->input->post('tipocliente_id'),
 					'categoriaclie_id' => $this->input->post('categoriaclie_id'),
 					'cliente_codigo' => $this->input->post('cliente_codigo'),
-					'categoriacliezona_id' => $this->input->post('categoriacliezona_id'),
+					'zona_id' => $this->input->post('zona_id'),
 					'cliente_nombre' => $this->input->post('cliente_nombre'),
 					'cliente_ci' => $this->input->post('cliente_ci'),
 					'cliente_direccion' => $this->input->post('cliente_direccion'),
@@ -284,7 +285,7 @@ class Cliente extends CI_Controller{
 					'cliente_foto' => $foto,
 					'cliente_email' => $this->input->post('cliente_email'),
 					'cliente_nombrenegocio' => $this->input->post('cliente_nombrenegocio'),
-					'cliente_aniversario' => $mifecha,
+					'cliente_aniversario' => $this->input->post('cliente_aniversario'),
 					'cliente_latitud' => $this->input->post('cliente_latitud'),
 					'cliente_longitud' => $this->input->post('cliente_longitud'),
 					'cliente_nit' => $this->input->post('cliente_nit'),
@@ -505,6 +506,52 @@ class Cliente extends CI_Controller{
         }
     }
     
+    
+    /*
+    * buscar clientes
+    */
+    function buscarclientes_usuario()
+    {
+        if ($this->session->userdata('logged_in')) {
+            $session_data = $this->session->userdata('logged_in');
+            if($session_data['tipousuario_id']==1) {
+                
+                $usuario_id = $session_data['usuario_id'];
+
+        if ($this->input->is_ajax_request()) {
+            
+            $parametro = $this->input->post('parametro');   
+            $tipo = $this->input->post('tipo');
+            
+            if($tipo == 1){ //busque de solo mis clientes
+                $condicion = " c.usuario_id = ".$usuario_id;
+             
+            }
+            else{
+                $condicion = " "; //busqueda de todos los clientes
+
+            }
+            
+            
+            if ($parametro!=""){
+            $datos = $this->Cliente_model->get_cliente_por_usuario($parametro,$condicion);
+            echo json_encode($datos);
+            }
+            else echo json_encode(null);
+        }
+        else
+        {                 
+            show_404();
+        }
+        }
+            else{
+                redirect('alerta');
+            }
+        } else {
+            redirect('', 'refresh');
+        }
+    }
+    
     /*
     * buscar clientes
     */
@@ -614,13 +661,13 @@ class Cliente extends CI_Controller{
     /*
      * Adding a new cliente
      */
-    function cambiarcliente()
+    function cambiarcliente($cliente_id,$pedido_id,$cliente_nit,$cliente_razon)
     {   
-        $cliente_id = $this->input->post('cliente_id');
-        $pedido_id = $this->input->post('pedido_id');
-        $cliente_nit = $this->input->post('cliente_nit');
-        $cliente_razon = $this->input->post('cliente_razon');
-                  
+//        $cliente_id = $this->input->post('cliente_id');
+//        $pedido_id = $this->input->post('pedido_id');
+//        $cliente_nit = $this->input->post('cliente_nit');
+//        $cliente_razon = $this->input->post('cliente_razon');
+//                  
 
         $this->load->model('Pedido_model');
         $sql = "update cliente set cliente_nit = '".$cliente_nit."',".
