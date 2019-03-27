8e375bb2eb72ceb0b8f537357fb7de61f77157a0
Mario escobar <escobar_m_98@hotmail.com>
2019-03-27 17:14:34 -0400

    Producto, vendedor puede crear producto desde compras; añadio ing-egr..

--- application/controllers/Producto.php
+++ application/controllers/Producto.php
@@ -493,7 +493,7 @@ class Producto extends CI_Controller{
  {
      if ($this->session->userdata('logged_in')) {
             $session_data = $this->session->userdata('logged_in');
-            if($session_data['tipousuario_id']==1) {
+            if($session_data['tipousuario_id']>=1 and $session_data['tipousuario_id']<=3) {
                 $data = array(
                     'page_title' => 'Admin >> Mi Cuenta'
                 );
