d0e7e64a3a25235fd5f6252c6987ac8516b8a132
alejandro <ale.hv10@gmail.com>
2019-04-04 15:49:27 -0400

    dosificacion arreglo

--- application/views/admin/dosificacion.php
+++ application/views/admin/dosificacion.php
@@ -1,7 +1,11 @@
+<?php if($diasdo['dias'] < 0){ ?>
 <div class="info-box bg-red">
                 <span class="info-box-icon"><i class="ion-alert-circled"></i></span>
+
                 <div class="info-box-content">
-                  <span class="info-box-text"><font size="4">LA DOSIFICACION VENCERA EN: <font size="5"><b><?php echo $diasdo['dias']; ?></b></font> DIAS</font></span>
+                  
+                  <span class="info-box-text"><font size="4"><b>LA DOSIFICACION YA ESTA VENCIDA </b></font></span>
+                
                   <span class="info-box-number"></span>
                   <div class="progress">
                     <div class="progress-bar" style="width: 100%"></div>
@@ -11,4 +15,21 @@
                   </span>
                 </div><!-- /.info-box-content -->
               </div>
-              
+<?php } else {  ?>
+<div class="info-box bg-red">
+                <span class="info-box-icon"><i class="ion-alert-circled"></i></span>
+
+                <div class="info-box-content">
+                               
+                  <span class="info-box-text"><font size="4">LA DOSIFICACION VENCERA EN: <font size="5"><b><?php echo $diasdo['dias']; ?></b></font> DIAS</font></span>
+                
+                  <span class="info-box-number"></span>
+                  <div class="progress">
+                    <div class="progress-bar" style="width: 100%"></div>
+                  </div>
+                  <span class="progress-description">
+                    No podra emitir facturas
+                  </span>
+                </div><!-- /.info-box-content -->
+              </div>  
+<?php } ?>
