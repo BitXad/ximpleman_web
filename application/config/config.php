3be4349758156d18e4cacc4a1cc5b287a27d6ce4
Mario escobar <escobar_m_98@hotmail.com>
2019-05-15 18:32:02 -0400

    reporte ingresos/egresos y config 4 horas
    4 horas de session

--- application/config/config.php
+++ application/config/config.php
@@ -375,7 +375,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 86400;
+$config['sess_expiration'] = 14400;
 $config['sess_save_path'] = 'ci_session';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
