08a502656e3130b1a92f5290f6995da1de169da9
carlos <masterbity@gmail.com>
2019-06-08 12:08:24 -0400

    08.06.2019 usuario_destino

---
+++ application/views/usuario_destino/index.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Usuario Destino Listing</h3>
+            	<div class="box-tools">
+                    <a href="<?php echo site_url('usuario_destino/add'); ?>" class="btn btn-success btn-sm">Add</a> 
+                </div>
+            </div>
+            <div class="box-body">
+                <table class="table table-striped">
+                    <tr>
+						<th>Usuariodestino Id</th>
+						<th>Usuario Id</th>
+						<th>Destino Id</th>
+						<th>Actions</th>
+                    </tr>
+                    <?php foreach($usuario_destino as $u){ ?>
+                    <tr>
+						<td><?php echo $u['usuariodestino_id']; ?></td>
+						<td><?php echo $u['usuario_id']; ?></td>
+						<td><?php echo $u['destino_id']; ?></td>
+						<td>
+                            <a href="<?php echo site_url('usuario_destino/edit/'.$u['usuariodestino_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
+                            <a href="<?php echo site_url('usuario_destino/remove/'.$u['usuariodestino_id']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </table>
+                                
+            </div>
+        </div>
+    </div>
+</div>
