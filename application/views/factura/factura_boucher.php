01559db1db9ea15e0830dbefb349f8b84317ec29
Carlos Soto <r.carlos.soto@hotmail.com>
2020-06-06 09:56:02 -0400

    06.06.2020 para push

--- application/views/factura/factura_boucher.php
+++ application/views/factura/factura_boucher.php
@@ -116,7 +116,7 @@ border-bottom : 1px solid #aaa;*/
                     <font size="2" face="Arial"><b><?php echo $empresa[0]['empresa_nombre']; ?></b></font><br>
                     <font size="1" face="Arial narrow"><b><?php echo $empresa[0]['empresa_eslogan']; ?></b></font><br>                    
                     <!--<font size="1" face="Arial"><b><?php echo "De: ".$empresa[0]['empresa_propietario']; ?></b></font><br>-->
-                    <?php if (sizeof($empresa[0]['empresa_propietario'])>0){ ?>
+                    <?php if (isset($empresa[0]['empresa_propietario'])){ ?>
                     <font size="1" face="Arial"></b>
 
                         <?php  echo "<b> DE: ".$empresa[0]['empresa_propietario'] ; ?>
