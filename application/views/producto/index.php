954e5714cf415f60b5bcaac6fefe29500facaf43
Mario escobar <escobar_m_98@hotmail.com>
2020-06-07 08:03:14 -0400

    Producto: catalogo y lista de percios

--- application/views/producto/index.php
+++ application/views/producto/index.php
@@ -43,6 +43,7 @@
 <input type="hidden" name="parametro_modulorestaurante" id="parametro_modulorestaurante" value="<?php echo $parametro['parametro_modulorestaurante']; ?>" />
 <input type="hidden" name="formaimagen" id="formaimagen" value="<?php  echo $parametro['parametro_formaimagen']; ?>" />
 <input type="hidden" name="tipousuario_id" id="tipousuario_id" value="<?php  echo $tipousuario_id; ?>" />
+<input type="hidden" name="resproducto" id="resproducto" />
 <!--<input type="hidden" name="lapresentacion" id="lapresentacion" value='<?php /*echo json_encode($all_presentacion); ?>' />
 <input type="hidden" name="lamoneda" id="lamoneda" value='<?php echo json_encode($all_moneda); */ ?>' /> -->
 
@@ -60,7 +61,7 @@
             <center>
             
                 <br><br>
-                <font size="3" face="arial"><b>PRODUCTOS</b></font> <br>
+                <font size="3" face="arial"><b><span id="titcatalogo"></span>PRODUCTOS</b></font> <br>
                 
                 <font size="1" face="arial"><b><?php echo date("d/m/Y H:i:s"); ?></b></font> <br>
 
@@ -83,8 +84,8 @@
         <!--este es INICIO del BREADCRUMB buscador-->
 <!--        <div class="row">
             <ol class="breadcrumb">
-                <li><a href="<?php echo site_url('admin/dashb')?>"><i class="fa fa-dashboard"></i> Inicio</a></li>
-                <li><a href="<?php echo site_url('cliente')?>">Clientes</a></li>
+                <li><a href="<?php //echo site_url('admin/dashb')?>"><i class="fa fa-dashboard"></i> Inicio</a></li>
+                <li><a href="<?php //echo site_url('cliente')?>">Clientes</a></li>
                 <li class="active"><b>Productos: </b></li>
                 <input style="border-width: 0; background-color: #DEDEDE" id="encontrados" type="text"  size="5"  readonly="true">
             </ol>
@@ -165,17 +166,26 @@
     </div>
     <!---------------- BOTONES --------->
     <div class="col-md-4">
-        
-            <div class="box-tools text-center">
+        <div class="box-tools text-center">
             <a href="<?php echo site_url('producto/add'); ?>" class="btn btn-success btn-foursquarexs" title="Registrar nuevo Producto"><font size="5"><span class="fa fa-user-plus"></span></font><br><small>Registrar</small></a>
             <button data-toggle="modal" data-target="#modalbuscar" class="btn btn-warning btn-foursquarexs" onclick="tablaresultadosproducto(3)" title="Mostrar todos los Productos" ><font size="5"><span class="fa fa-search"></span></font><br><small>Ver Todos</small></button>
             <a href="<?php echo site_url('producto/existenciaminima'); ?>" class="btn btn-info btn-foursquarexs" target="_blank" ><font size="5" title="Productos con Existencia minima"><span class="fa fa-eye"></span></font><br><small>Exist. Min.</small></a>
             <?php
             if($rol[106-1]['rolusuario_asignado'] == 1){ ?>
             <a onclick="imprimir_producto()" class="btn btn-primary btn-foursquarexs"><font size="5" title="Imprimir Producto"><span class="fa fa-print"></span></font><br><small>Imprimir</small></a>
+            <table style="display: inline">
+                <tr><td>
+                <input class="btn" type="checkbox" name="escatalogo" id="escatalogo" title="Catalogo de Productos" onclick="catalogoproducto()" >
+                </td>
+                </tr>
+                <tr><td>
+                <input class="btn" type="checkbox" name="listaprecios" id="listaprecios" title="Lista de Precios" onclick="listaprecios()" >
+                </td>
+                </tr>
+            </table>
             <?php } ?>
             <!--<a href="" class="btn btn-info btn-foursquarexs"><font size="5"><span class="fa fa-cubes"></span></font><br><small>Productos</small></a>-->            
-    </div>
+        </div>
     </div>
     <!---------------- FIN BOTONES --------->
 </div>
@@ -189,12 +199,11 @@
             <div class="box-body  table-responsive">
                <table class="table table-condensed" id="mitabla" role="table">
                <!--<table role="table">-->
-                    <thead role="rowgroup">
-                        <tr role="row">
+                    <thead role="rowgroup" id="cabcatalogo">
+                        <!--<tr role="row">
                             <th  role="columnheader" >#</th>
                             <th  role="columnheader" >Nombre</th>
                             <th  role="columnheader" >Categoria|<br>Presentación</th>
-                            <!--<th  role="columnheader" style="width: 20%;" >Caracteristicas</th>-->
                             <th  role="columnheader" >Envase</th>
                             <th  role="columnheader" >Código|<br>Cód. Barra</th>
                             <th  role="columnheader" >Precio</th>
@@ -202,7 +211,7 @@
                             <th  role="columnheader" class="no-print">Estado</th>
                             <th  role="columnheader" class="no-print"></th>
                     
-                    </tr>
+                    </tr>-->
                     </thead>
                     <tbody class="buscar" id="tablaresultados" role="rowgroup">
                                          
--- resources/js/funciones_producto.js
+++ resources/js/funciones_producto.js
@@ -42,8 +42,9 @@ function formatofecha_hora_ampm(string){
 }
 
 //Tabla resultados de la busqueda en el index de producto
-function tablaresultadosproducto(limite)
-{
+function tablaresultadosproducto(limite){
+    $("#listaprecios").prop("checked", false);
+    $("#escatalogo").prop("checked", false);
     var controlador = "";
     var parametro = "";
     var categoriatext = "";
@@ -98,7 +99,8 @@ function tablaresultadosproducto(limite)
                 
                if (registros != null){
                    var formaimagen = document.getElementById('formaimagen').value;
-                   
+                   const myString = JSON.stringify(registros);
+                   $("#resproducto").val(myString);
                     /*var cont = 0;
                     var cant_total = 0;
                     var total_detalle = 0; */
@@ -267,8 +269,7 @@ function tablaresultadosproducto(limite)
                         html += "</tr>";
 
                    }
-                   
-                   
+                   cabecera_tabla();
                    $("#tablaresultados").html(html);
                    document.getElementById('loader').style.display = 'none';
             }
@@ -288,3 +289,342 @@ function tablaresultadosproducto(limite)
 
 }
 
+function catalogoproducto() {
+    $("#listaprecios").prop("checked", false);
+    $('#titcatalogo').text("CATALOGO DE ");
+    var base_url = document.getElementById('base_url').value;
+    //var checkBox = document.getElementById("myCheck");
+    var formaimagen = document.getElementById('formaimagen').value;
+    var catalogo = $('#escatalogo').is(':checked');
+    var respuesta = document.getElementById('resproducto').value;
+    var registros =  JSON.parse(respuesta);
+    var n = registros.length; //tamaño del arreglo de la consulta
+    if(catalogo){
+        var numcolumna = 3;
+        var inifila = "";
+        var finfila = "";
+        var contcol = 1;
+        chtml = "";
+        chtml += "<tr role='row'  style='width: 19cm !important'>";
+        chtml += "<th colspan='"+numcolumna+"'  role='columnheader' >CATALOGO DE PRODUCTOS</th>";
+        chtml += "</tr>";
+        html = "";
+        for (var i = 0; i < n ; i++){
+            if(contcol <= numcolumna){
+                if(contcol == 1){
+                    inifila ="<tr style='width: 19cm !important'>";
+                    finfila = "";
+                    contcol++;
+                    //bandfila = false;
+                }else if(i+1== n || contcol == 3){
+                    inifila = "";
+                    finfila ="</tr>";
+                    contcol = 1;
+                }else{
+                    inifila = "";
+                    finfila = "";
+                    contcol++;
+                }
+            }else{
+                contcol = 1;
+            }
+            
+            html += inifila;
+            html += "<td style='width: 300px; height: 150px'>";
+            //html += "<div style='width: 300px; height: 300px'>";
+            html += "<div>";
+            //html += "<div style='height: 300px !important'>";
+            var mimagen = "";
+            if(registros[i]["producto_foto"] != null && registros[i]["producto_foto"] !=""){
+                mimagen += "<a class='btn  btn-xs' data-toggle='modal' data-target='#mostrarimagen"+i+"' style='padding: 0px;'>";
+                mimagen += "<img src='"+base_url+"resources/images/productos/"+registros[i]["producto_foto"]+"' class='img img-"+formaimagen+"' width='100%' height='100%' />";
+                mimagen += "</a>";
+                //mimagen = nomfoto.split(".").join("_thumb.");77
+            }else{
+                mimagen = "<img src='"+base_url+"resources/images/productos/producto.jpg' class='img img-"+formaimagen+"' width='100%' height='100%' />";
+            }
+            html += mimagen;
+            //html += "</div>";
+            html += "<div style='padding-left: 4px'>";
+            var tamaniofont = 3;
+            if(registros[i]["producto_nombre"].length >50){
+                tamaniofont = 1;
+            }
+            html += "<b id='masgrande'><font size='"+tamaniofont+"' face='Arial'><b>"+registros[i]["producto_nombre"]+"</b></font></b><br>";
+            html += ""+registros[i]["producto_unidad"]+" | "+registros[i]["producto_marca"]+" | "+registros[i]["producto_industria"]+"";
+            if(registros[i]["destino_id"] > 0){
+                    html +="<br><b>DESTINO:</b> "+registros[i]['destino_nombre'];
+            }
+            /*if(parametro_modulo == 2){
+                html +="<br>Principio Activo: "+registros[i]['producto_principioact'];
+                html +="<br>Acción Terapeutica: "+registros[i]['producto_accionterap'];
+            }
+
+            html += caracteristica;*/
+            html += "";
+            html += "</div>";
+            html += "</div>";
+            html += "</td>";
+            html += finfila;
+        }
+        $("#cabcatalogo").html(chtml);
+        $("#tablaresultados").html(html);
+    }else{
+        busqueda_inicial();
+    }
+    //cabcatalogo
+}
+function listaprecios() {
+    $("#escatalogo").prop("checked", false);
+    $('#titcatalogo').text("LISTA DE PRECIOS DE ");
+    var base_url = document.getElementById('base_url').value;
+    //var checkBox = document.getElementById("myCheck");
+    var formaimagen = document.getElementById('formaimagen').value;
+    var catalogo = $('#listaprecios').is(':checked');
+    var respuesta = document.getElementById('resproducto').value;
+    var registros =  JSON.parse(respuesta);
+    var n = registros.length; //tamaño del arreglo de la consulta
+    if(catalogo){
+        chtml = "";
+        chtml += "<tr role='row'  style='width: 19cm !important'>";
+        chtml += "<th role='columnheader' >#</th>";
+        chtml += "<th role='columnheader' >Nombre</th>";
+        chtml += "<th role='columnheader' >Unidad</th>";
+        chtml += "<th role='columnheader' >Cod. Barra</th>";
+        chtml += "<th role='columnheader' >Precio</th>";
+        chtml += "</tr>";
+        html = "";
+        for (var i = 0; i < n ; i++){
+            html += "<tr>";
+            html += "<td class='text-center'>"+(i+1)+"</td>";
+            html += "<td>";
+            html += "<div style='padding-left: 4px'>";
+            var tamaniofont = 3;
+            if(registros[i]["producto_nombre"].length >50){
+                tamaniofont = 1;
+            }
+            html += "<b id='masgrande'><font size='"+tamaniofont+"' face='Arial'><b>"+registros[i]["producto_nombre"]+"</b></font></b><br>";
+            html += ""+registros[i]["producto_unidad"]+" | "+registros[i]["producto_marca"]+" | "+registros[i]["producto_industria"]+"";
+            if(registros[i]["destino_id"] > 0){
+                    html +="<br><b>DESTINO:</b> "+registros[i]['destino_nombre'];
+            }
+            /*if(parametro_modulo == 2){
+                html +="<br>Principio Activo: "+registros[i]['producto_principioact'];
+                html +="<br>Acción Terapeutica: "+registros[i]['producto_accionterap'];
+            }
+
+            html += caracteristica;*/
+            html += "";
+            html += "</div>";
+            html += "</td>";
+            html += "<td class='text-center'>";
+            html += registros[i]["producto_unidad"];
+            html += "</td>";
+            html += "<td class='text-right'>";
+            html += registros[i]["producto_codigobarra"];
+            html += "</td>";
+            html += "<td class='text-right'>";
+            html += registros[i]["producto_precio"];
+            html += "</td>";
+            html += "</tr>";
+        }
+        $("#cabcatalogo").html(chtml);
+        $("#tablaresultados").html(html);
+    }else{
+        busqueda_inicial();
+    }
+    //cabcatalogo
+}
+
+function cabecera_tabla() {
+    chtml = "";
+    chtml += "<tr role='row'>";
+    chtml += "<th  role='columnheader' >#</th>";
+    chtml += "<th  role='columnheader' >Nombre</th>";
+    chtml += "<th  role='columnheader' >Categoria|<br>Presentación</th>";
+    chtml += "<th  role='columnheader' >Envase</th>";
+    chtml += "<th  role='columnheader' >Código|<br>Cód. Barra</th>";
+    chtml += "<th  role='columnheader' >Precio</th>";
+    chtml += "<th  role='columnheader' >Moneda</th>";
+    chtml += "<th  role='columnheader' class='no-print'>Estado</th>";
+    chtml += "<th  role='columnheader' class='no-print'></th>";
+    chtml += "</tr>";
+    $("#cabcatalogo").html(chtml);
+}
+function busqueda_inicial() {
+    $('#titcatalogo').text("");
+    var base_url = document.getElementById('base_url').value;
+    //var checkBox = document.getElementById("myCheck");
+    var formaimagen = document.getElementById('formaimagen').value;
+    //var catalogo = $('#listaprecios').is(':checked');
+    var respuesta = document.getElementById('resproducto').value;
+    var registros =  JSON.parse(respuesta);
+    var n = registros.length; //tamaño del arreglo de la consulta
+    var parametro_modulo = document.getElementById('parametro_modulorestaurante').value;
+    var tipousuario_id = document.getElementById('tipousuario_id').value;
+    html  = "";
+    cabecera_tabla();
+    for (var i = 0; i < n ; i++){
+//                        html += "<td>";
+        var caracteristica = "";
+        if(registros[i]["producto_caracteristicas"] != null){
+            caracteristica = "<div style='word-wrap: break-word !important; max-width: 400px !important; white-space: normal'>"+registros[i]["producto_caracteristicas"]+"</div>";
+        }
+//                        html+= caracteristica+"</td>";                        
+
+        html += "<tr>";
+
+        html += "<td>"+(i+1)+"</td>";
+        html += "<td>";
+        html += "<div id='horizontal'>";
+        html += "<div id='contieneimg'>";
+        var mimagen = "";
+        if(registros[i]["producto_foto"] != null && registros[i]["producto_foto"] !=""){
+            mimagen += "<a class='btn  btn-xs' data-toggle='modal' data-target='#mostrarimagen"+i+"' style='padding: 0px;'>";
+            mimagen += "<img src='"+base_url+"resources/images/productos/thumb_"+registros[i]["producto_foto"]+"' class='img img-"+formaimagen+"' width='50' height='50' />";
+            mimagen += "</a>";
+            //mimagen = nomfoto.split(".").join("_thumb.");77
+        }else{
+            mimagen = "<img src='"+base_url+"resources/images/productos/thumb_image.png' class='img img-"+formaimagen+"' width='50' height='50' />";
+        }
+        html += mimagen;
+        html += "</div>";
+        html += "<div style='padding-left: 4px'>";
+        var tamaniofont = 3;
+        if(registros[i]["producto_nombre"].length >50){
+            tamaniofont = 1;
+        }
+        html += "<b id='masgrande'><font size='"+tamaniofont+"' face='Arial'><b>"+registros[i]["producto_nombre"]+"</b></font></b><br>";
+        html += ""+registros[i]["producto_unidad"]+" | "+registros[i]["producto_marca"]+" | "+registros[i]["producto_industria"]+"";
+        if(registros[i]["destino_id"] > 0){
+                html +="<br><b>DESTINO:</b> "+registros[i]['destino_nombre'];
+        }
+        if(parametro_modulo == 2){
+            html +="<br>Principio Activo: "+registros[i]['producto_principioact'];
+            html +="<br>Acción Terapeutica: "+registros[i]['producto_accionterap'];
+        }
+
+        html += caracteristica;
+        html += "";
+        html += "</div>";
+        html += "</div>";
+        html += "</td>";
+        var escategoria="";
+        if(registros[i]["categoria_id"] == null || registros[i]["categoria_id"] == 0 || registros[i]["categoria_id"] ==""){
+            escategoria = "No definido";
+        }else{
+            escategoria = registros[i]["categoria_nombre"];
+        }
+        var essubcategoria="";
+        if(registros[i]["subcategoria_id"] == null || registros[i]["subcategoria_id"] == 0 || registros[i]["subcategoria_id"] ==""){
+            essubcategoria = "No definido";
+        }else{
+            essubcategoria = registros[i]["subcategoria_nombre"];
+        }
+        var esmoneda="";
+        if(registros[i]["moneda_id"] == null || registros[i]["moneda_id"] == 0 || registros[i]["moneda_id"] == ""){ 
+            esmoneda = "No definido";
+        }else{
+            esmoneda = registros[i]["moneda_descripcion"];
+        }
+        html += "<td><b>CATEGORIA: </b>"+escategoria+"<br><b>SUB CATEGORIA: </b>"+essubcategoria+"<br>";
+        html += "<b>UNIDAD: </b>"+registros[i]["producto_unidad"]+"<br>";
+        html += "<b>CANT. MIN.: </b>";
+        var cantmin= 0;
+        if(registros[i]["producto_cantidadminima"] != null || registros[i]["producto_cantidadminima"] ==""){
+            cantmin = registros[i]["producto_cantidadminima"];
+        }
+        html += cantmin+"</td>";
+
+        html += "<td>";
+        var sinconenvase = "";
+        var nombreenvase = "";
+        var costoenvase  = "";
+        var precioenvase = "";
+        if(registros[i]["producto_envase"] == 1){
+            sinconenvase = "Con Envase Retornable"+"<br>";
+            if(registros[i]["producto_nombreenvase"] != "" || registros[i]["producto_nombreenvase"] != null){
+                nombreenvase = registros[i]["producto_nombreenvase"]+"<br>";
+                costoenvase  = "Costo:  "+Number(registros[i]["producto_costoenvase"]).toFixed(2)+"<br>";
+                precioenvase = "Precio: "+Number(registros[i]["producto_precioenvase"]).toFixed(2);
+            }
+        }else{
+            sinconenvase = "Sin Envase Retornable";
+        }
+        html += sinconenvase;
+        html += nombreenvase;
+        html += costoenvase;
+        html += precioenvase;
+        html += "</td>";
+        var codbarras = "";
+        if(!(registros[i]["producto_codigobarra"] == null)){
+            codbarras = registros[i]["producto_codigobarra"];
+        }
+        html += "<td>"+registros[i]["producto_codigo"]+"<br>"+ codbarras +"</td>";
+        html += "<td>";
+        if(tipousuario_id == 1){
+            html += "<b>COMPRA: </b>"+registros[i]["producto_costo"]+"<br>";
+        }
+            html += "<b>VENTA: </b>"+registros[i]["producto_precio"]+"<br>";
+            html += "<b>COMISION (%): </b>"+registros[i]["producto_comision"];
+            html += "</td>";
+        html += "<td><b>MONEDA: </b>"+esmoneda+"<br>";
+        html += "<b>T.C.: </b>";
+        var tipocambio= 0;
+        if(registros[i]["producto_tipocambio"] != null){ tipocambio = registros[i]["producto_tipocambio"]; }
+        html += tipocambio+"</td>";
+        html += "<td class='no-print' style='background-color: #"+registros[i]["estado_color"]+"'>"+registros[i]["estado_descripcion"]+"</td>";
+        html += "<td class='no-print'>";
+        html += "<a href='"+base_url+"producto/edit/"+registros[i]["miprod_id"]+"' target='_blank' class='btn btn-info btn-xs' title='Modificar Información'><span class='fa fa-pencil'></span></a>";
+        html += "<a href='"+base_url+"imagen_producto/catalogoprod/"+registros[i]["miprod_id"]+"' class='btn btn-success btn-xs' title='Catálogo de Imagenes' ><span class='fa fa-image'></span></a>";
+        html += "<a class='btn btn-danger btn-xs' data-toggle='modal' data-target='#myModal"+i+"' title='Eliminar'><span class='fa fa-trash'></span></a>";
+        html += "<a href='"+base_url+"producto/productoasignado/"+registros[i]["miprod_id"]+"' class='btn btn-soundcloud btn-xs' title='Ver si esta asignado a subcategorias' target='_blank' ><span class='fa fa-list'></span></a>";
+        html += "<!------------------------ INICIO modal para confirmar eliminación ------------------->";
+        html += "<div class='modal fade' id='myModal"+i+"' tabindex='-1' role='dialog' aria-labelledby='myModalLabel"+i+"'>";
+        html += "<div class='modal-dialog' role='document'>";
+        html += "<br><br>";
+        html += "<div class='modal-content'>";
+        html += "<div class='modal-header'>";
+        html += "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>x</span></button>";
+        html += "</div>";
+        html += "<div class='modal-body'>";
+        html += "<!------------------------------------------------------------------->";
+        html += "<h3><b> <span class='fa fa-trash'></span></b>";
+        html += "¿Desea eliminar el Producto <b> "+registros[i]["producto_nombre"]+"</b> ?";
+        html += "</h3>";
+        html += "<!------------------------------------------------------------------->";
+        html += "</div>";
+        html += "<div class='modal-footer aligncenter'>";
+        html += "<a href='"+base_url+"producto/remove/"+registros[i]["miprod_id"]+"' class='btn btn-success'><span class='fa fa-check'></span> Si </a>";
+        html += "<a href='#' class='btn btn-danger' data-dismiss='modal'><span class='fa fa-times'></span> No </a>";
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+        html += "<!------------------------ FIN modal para confirmar eliminación ------------------->";
+        html += "<!------------------------ INICIO modal para MOSTRAR imagen REAL ------------------->";
+        html += "<div class='modal fade' id='mostrarimagen"+i+"' tabindex='-1' role='dialog' aria-labelledby='mostrarimagenlabel"+i+"'>";
+        html += "<div class='modal-dialog' role='document'>";
+        html += "<br><br>";
+        html += "<div class='modal-content'>";
+        html += "<div class='modal-header'>";
+        html += "<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>x</span></button>";
+        html += "<font size='3'><b>"+registros[i]["producto_nombre"]+"</b></font>";
+        html += "</div>";
+        html += "<div class='modal-body'>";
+        html += "<!------------------------------------------------------------------->";
+        html += "<img style='max-height: 100%; max-width: 100%' src='"+base_url+"resources/images/productos/"+registros[i]["producto_foto"]+"' />";
+        html += "<!------------------------------------------------------------------->";
+        html += "</div>";
+
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+        html += "<!------------------------ FIN modal para MOSTRAR imagen REAL ------------------->";
+        html += "</td>";
+
+        html += "</tr>";
+
+    }
+    $("#tablaresultados").html(html);
+}
