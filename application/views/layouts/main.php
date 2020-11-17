ff8ec14f7df110a31f3dd23352f947e3f7ce3cb4
Mario escobar <escobar_m_98@hotmail.com>
2020-11-17 14:10:20 -0400

    detalle

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -557,7 +557,7 @@
                         if($rolusuario[24-1]['rolusuario_asignado'] == 1){
                         ?>
                         <li>
-                            <a href="<?php echo site_url('clasificador/inventario');?>"><i class="fa fa-cubes"></i>Clasificador Inventario</a>
+                            <a href="<?php echo site_url('clasificador/inventario');?>"><i class="fa fa-cubes"></i>Inventario Clasificador</a>
                         </li>
                         <?php
                         } ?>
