186bb201b84f7ad3745108ab49386856a3d527cb
Mario escobar <escobar_m_98@hotmail.com>
2021-02-09 11:33:58 -0400

    09.02.2021 zona.. orden alfabetico

--- application/models/Categoria_clientezona_model.php
+++ application/models/Categoria_clientezona_model.php
@@ -98,7 +98,7 @@ class Categoria_clientezona_model extends CI_Model
             WHERE
                 cc.estado_id = e.estado_id
                 
-                ORDER BY cc.zona_id ASC
+                ORDER BY cc.zona_nombre ASC
 
         ")->result_array();
 
