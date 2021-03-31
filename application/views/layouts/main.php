d7306e11aaaeffd08dd28525d879c1a76ed1f711
Mario escobar <escobar_m_98@hotmail.com>
2021-03-31 11:01:40 -0400

    para push

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -562,7 +562,7 @@
                             <a href="<?php echo site_url('reportes/ventaproducto');?>"><i class="fa fa-product-hunt"></i>Ventas por Producto</a>
                         </li>
                         <li>
-                            <a href="<?php echo site_url('reportes/ventacategoria_pagrupado');?>"><i class="fa fa-certificate"></i>Producto Agrupado/Categoria</a>
+                            <a href="<?php echo site_url('reportes/ventacategoria_pagrupado');?>"><i class="fa fa-certificate"></i>P. Agrupado/Categoria</a>
                         </li>
                         <li>
                             <a href="<?php echo site_url('reportes/ventapagrupado');?>"><i class="fa fa-certificate"></i>Producto Agrupado</a>
