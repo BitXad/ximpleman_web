5448cde4887684be5b2ac965c8964290c8bdac39
douglas quisbert <duglas3093@gmail.com>
2022-01-14 18:44:28 -0400

    14.01.2022 reporte de mora (ventas)

---
+++ resources/js/reporte_mora.js
@@ -0,0 +1,61 @@
+var base_url;
+$(document).on("ready",function(){
+    base_url = document.getElementById('base_url').value;
+    tabla_moras();
+});
+
+function tabla_moras() {
+    let usuario = $('#buscarusuario_id').val();
+    let controlador = `${base_url}reportes/get_moras`
+    console.log(controlador)
+    $.ajax({
+        url: controlador,
+        type: "POST",
+        cache: false,
+        data:{
+            usuario: usuario
+        },
+        success: (result) => {
+            let html = ``;
+            let i = 1;
+            let moras = JSON.parse(result);
+            let total_capital = 0;
+            let total_intereses = 0;
+            let total_mora = 0;
+            let total_deuda = 0;
+            for(let mora of moras){
+                let total = parseFloat(mora['credito_monto']) + parseFloat(mora['cuota_interes']) + parseFloat(mora['multa'])
+
+                html += `<tr>
+                            <td>${i}</td>
+                            <td>${mora['cliente_nombre']}<sub>[${mora['credito_id']}]</sub></td>
+                            <td class="text-center">${mora['venta_id']}</td>
+                            <td class="text-center">${mora['dias_mora']}</td>
+                            <td class="text-center">${mora['credito_monto']}</td>
+                            <td class="text-center">${mora['cuota_interes']}</td>
+                            <td class="text-center">${parseFloat(mora['multa']).toFixed(2)}</td>
+                            <td class="text-center">${parseFloat(total).toFixed(2)}</td>
+                        </tr>`
+                i++;
+                total_capital += parseFloat(mora['credito_monto']);
+                total_intereses += parseFloat(mora['cuota_interes']);
+                total_mora += parseFloat(parseFloat(mora['multa']).toFixed(2));
+                total_deuda += parseFloat(total);
+            }
+            html += `<tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th>${parseFloat(total_capital).toFixed(2)}</th>
+                        <th>${parseFloat(total_intereses).toFixed(2)}</th>
+                        <th>${parseFloat(total_mora).toFixed(2)}</th>
+                        <th>${parseFloat(total_deuda).toFixed(2)}</th>
+                    </tr>`
+            $("#tablaMora").html(html);
+        },
+        error: () => {
+            alert("Error al obtener las moras")
+        }
+    })
+}
