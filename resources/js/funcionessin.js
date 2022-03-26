0f25c4c2e95f57879f4fb8c3030cced891b4da72
Mario Escobar <escobar_m_98@hotmail.com>
2022-03-26 11:33:08 -0400

    26.03.2022 Cuiss Masivo falta

--- resources/js/funcionessin.js
+++ resources/js/funcionessin.js
@@ -155,3 +155,82 @@ function verificarComunicacion(){
         }); 
     
 }
+
+function solicitudCuisMasivo(){
+    
+    var base_url = document.getElementById('base_url').value;
+    var controlador = base_url+'dosificacion/cuis';
+    var opcion = confirm("Esta a punto de generar el C.U.I.S., el cual reamplazara el existente...! \n ¿Desea Continuar?");
+    
+    if (opcion == true) {
+    
+        $.ajax({url:controlador,
+                type:"POST",
+                data:{},
+                success:function(respuesta){
+                    var registros = JSON.parse(respuesta);
+                    let transaccion = registros.RespuestaCuis.transaccion;
+
+                    if(transaccion == true){
+                       almacenar_cuis((registros['RespuestaCuis']));
+                    }
+                    else{
+                        
+                        let transaccion = registros.RespuestaCuis.mensajesList.codigo;
+                        let descripcion = registros.RespuestaCuis.mensajesList.descripcion;
+                        alert("ERROR: "+transaccion+" "+descripcion);
+                    }
+
+                },
+                error:function(respuesta){
+                    alert("Algo salio mal; por favor verificar sus datos!.");
+                }                
+        }); 
+    }
+}
+
+function solicitudCufdMasivo(){
+    
+    var base_url = document.getElementById('base_url').value;
+    var controlador = base_url+'dosificacion/cufd';
+    var opcion = confirm("Esta a punto de generar el C.U.F.D., el cual reamplazara el existente...! \n ¿Desea Continuar?");
+    
+    if (opcion == true) {
+    
+        $.ajax({url:controlador,
+                type:"POST",
+                data:{},
+                success:function(respuesta){
+                    var registros = JSON.parse(respuesta);
+                    /*console.log(registros);
+                    console.log(registros.RespuestaVerificarNit.mensajesList.codigo);
+                    console.log(registros.RespuestaVerificarNit.mensajesList.descripcion);
+                    console.log(registros.RespuestaVerificarNit.transaccion);*/
+                    //let elcodigo = registros.RespuestaVerificarNit.mensajesList.codigo;
+                    let codigo = registros.RespuestaCufd.codigo;
+                    let codigoControl = registros.RespuestaCufd.codigoControl;
+                    let direccion = registros.RespuestaCufd.direccion;
+                    let fechaVigencia = registros.RespuestaCufd.fechaVigencia;
+                    let transaccion = registros.RespuestaCufd.transaccion;
+
+
+                    if(transaccion == true){
+                       // $("#modal_mensajeadvertencia").modal("show");
+                       almacenar_cufd((registros['RespuestaCufd']));
+                    }
+                    else{
+                        alert("Algo fallo...!!");
+                    }
+
+                    //alert("hola");
+                    /*if (registros[0]!=null){ //Si el cliente ya esta registrado  en el sistema
+
+                    }*/
+
+                },
+                error:function(respuesta){
+                    alert("Algo salio mal; por favor verificar sus datos!.");
+                }                
+        }); 
+    }
+}
