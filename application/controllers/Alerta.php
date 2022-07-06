95669b4052161f36199f74aebbdd8d69bf20995c
Mario Escobar <escobar_m_98@hotmail.com>
2022-07-06 10:27:07 -0400

    06.07.2022 control de token delegado al iniciar sistema

--- application/controllers/Alerta.php
+++ application/controllers/Alerta.php
@@ -12,8 +12,8 @@ class Alerta extends CI_Controller{
              '<br><a href="'.base_url('admin/dashb/logout').'" class="btn btn-info btn-xs">Cerrar Sesion</a><br>    <a href="javascript: history.go(-1)" class="btn btn-success">Volver atrás</a>';
     }
 
-public function dosificacion()
-{
+    public function dosificacion()
+    {
         $dosif="SELECT DATEDIFF(dosificacion_fechalimite, CURDATE()) as dias FROM dosificacion WHERE dosificacion_id = 1";
                 $dosificacion = $this->db->query($dosif)->row_array();
 
@@ -21,6 +21,16 @@ public function dosificacion()
         $data['_view'] = 'admin/dosificacion';
         $this->load->view('layouts/main',$data);
    
-}
+    }
+    public function token()
+    {
+        $dosif="SELECT DATEDIFF(token_fechahasta, CURDATE()) as dias FROM token WHERE estado_id = 1 order by token_id desc limit 1";
+                $token = $this->db->query($dosif)->row_array();
+
+        $data['diasdo'] = $token;
+        $data['_view'] = 'admin/token';
+        $this->load->view('layouts/main',$data);
+   
+    }
 }
 
---
+++ application/views/admin/token.php
@@ -0,0 +1,46 @@
+<?php if($diasdo['dias'] < 0){ ?>
+<div class="box-body table-responsive">
+  <table class="table table-striped table-condensed" >
+    <td>
+<div class="info-box bg-red">
+                <span class="info-box-icon"><i class="ion-alert-circled"></i></span>
+
+                <div class="info-box-content">
+                  
+                  <span class="info-box-text"><font size="4"><b>EL TOKEN DELEGADO YA ESTA VENCIDO </b></font></span>
+                
+                  <span class="info-box-number"></span>
+                  <div class="progress">
+                    <div class="progress-bar" style="width: 100%"></div>
+                  </div>
+                  <span class="progress-description">
+                    No podra emitir facturas
+                  </span>
+                </div><!-- /.info-box-content -->
+              </div></td>
+              </table> 
+            </div>
+<?php } else {  ?>
+<div class="box-body table-responsive">
+  <table class="table table-striped table-condensed" style="font-family: Arial;">
+    <td>
+        <div class="info-box bg-red">
+                <span class="info-box-icon"><i class="ion-alert-circled"></i></span>
+
+                <div class="info-box-content">
+                               
+                    <span class="info-box-text"><font size="4">EL TOKEN DELEGADO VENCERA EN: <font size="5"><b><?php echo $diasdo['dias']; ?></b></font> DIAS</font></span>
+                
+                  <span class="info-box-number"></span>
+                  <div class="progress">
+                    <div class="progress-bar" style="width: 100%"></div>
+                  </div>
+                  <span class="progress-description">
+                    No podrá emitir facturas
+                  </span>
+                </div><!-- /.info-box-content -->
+        </div> 
+    </td>
+    </table> 
+</div>
+<?php } ?>
