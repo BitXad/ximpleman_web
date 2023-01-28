47a8051734f93e0a9d182ec10899ddbe3bffb462
bitXad <r.carlos.soto@hotmail.com>
2023-01-28 10:18:11 -0400

    28.01.2023 Titulo en website

--- application/views/web/login.php
+++ application/views/web/login.php
@@ -7,7 +7,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html>
 <head>
-<title>Ximpleman | Sistema Integral de Ventas :: Web</title>
+<title><?php echo $sistema["sistema_nombre"]." ".$sistema["sistema_version"]; ?></title>
+<!--<title>Ximpleman | Sistema Integral de Ventas :: Web</title>-->
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
