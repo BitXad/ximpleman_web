e26193ae8faab08eb16d8090baa37457341eb50c
bitXad <r.carlos.soto@hotmail.com>
2023-01-30 12:14:51 -0400

    12.14 Cambios en login

--- application/views/login/singin.php
+++ application/views/login/singin.php
@@ -85,7 +85,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
           <!--<h2 class="text-center"><?php echo $empresa[0]["empresa_nombre"] ?></h2>-->
           <center>
               <font size="5" face="Arial black"><b><?php echo $empresa[0]["empresa_nombre"] ?></b></font><br>              
-                <img src="<?php echo base_url('resources/images/empresas/'.$empresa[0]["empresa_imagen"].''); ?>"  style="width:80px;height:80px">
+                <img src="<?php echo base_url('resources/images/empresas/'.$empresa[0]["empresa_imagen"].''); ?>"  style="width:100px;height:50px">
                 <br><font size="4" face="Arial black"><b><?php echo $sistema["sistema_nombre"]." ".$sistema["sistema_version"]; ?></b></font>
           </center>
         </div>
